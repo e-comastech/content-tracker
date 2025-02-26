0209da9a68b245c05addb0d5e555a2bc85bba234
Claudiu Clement <analytics@e-comas.com>
2025-02-26 19:06:50 +0200

    Fix asset paths and main page

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,12 @@ export default defineConfig({
   base: '/cct/',
   build: {
     outDir: 'dist/cct',
-    assetsDir: 'assets'
-  }
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    }
+  },
+  publicDir: resolve(__dirname, 'public'),
 });
