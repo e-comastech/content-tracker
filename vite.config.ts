c4f99825b3f513e424b1f0a30178858cf3786baa
Claudiu Clement <analytics@e-comas.com>
2025-02-26 18:10:05 +0200

    Fix React app loading and logo path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,10 @@ export default defineConfig({
         assetFileNames: 'assets/[name].[hash][extname]',
         chunkFileNames: 'assets/[name].[hash].js',
         entryFileNames: 'assets/[name].[hash].js',
+      },
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        landing: resolve(__dirname, 'public/index.html'),
       },
     },
   }
